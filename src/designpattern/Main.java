8402b1c035efc910825a31f05eb9e5866071e9b2
Mahrudin11 <1810817210010@mhs.ulm.ac.id>
2020-11-15 21:33:14 +0800

    fix

---
+++ src/designpattern/Main.java
@@ -0,0 +1,13 @@
+package designpattern;
+
+//class main agar program dapat di run
+public class Main {
+    public static void main(String[] args){
+        
+        System.out.println("Masukkan panjang dan Lebar Bangun Segi Empat: ");
+        Pola1 pola1 = new Pola1(); 
+        pola1.segiempat(); //pemanggilan method segiempat pada class TemplateMethode
+        
+    }
+}   
+ 
